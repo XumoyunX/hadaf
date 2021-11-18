8ec75a39d1cf3a196aa495bee06f02274d5aa62e
XumoyunX <xumoyuntoxtayev19@gmail.com>
2021-11-18 17:26:25 +0500

    sasa

--- hadaf/settings.py
+++ hadaf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-18-t(2$h-ang#fal^-vtjai3001wge$wv1gk$5abk7w!#@f#t5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
